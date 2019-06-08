db172089b988081a10876a21851097bd066ba1e2
Jaleesa McCrea <jmccrea01@gmail.com>
2019-06-07 20:13:33 -0400

    j

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,14 +1,18 @@
-let age = 25;
-let year = 2019;
-console.log(age, year);
-age = 30;
+// template stings
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
 
-console.log(30);
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 
-const points = 100;
-console.log(points);
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
 
+// creating html templates
+let html =
+`<h2>${title}</h2>
+<p>By ${author}</p>
+<span>This blog has ${likes} likes</span>
+`;
 
-var score =75;
-
-console.log(score);
+console.log(html);
