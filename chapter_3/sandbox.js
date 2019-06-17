a62380c03a68d1ee3bef0bc75c835cb97a6b3f7d
Jaleesa McCrea <jmccrea01@gmail.com>
2019-06-17 14:46:44 -0400

    mmm

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,12 @@
+// while loops
+
+// while loops can't have a declaration for i inside it
+
+const names = ['shaun', 'mario', 'luigi'];
+
+let i = 0;
+while( i < names.length){
+    console.log(names[i]);
+    i++;
+}
+
